a905c8ec8628ff5798a6a7902151aaad0bdf1ed6
dennislopes <dennislopes@gmail.com>
2023-02-16 00:14:24 -0300

    Update main.tf

--- examples/simple_project/main.tf
+++ examples/simple_project/main.tf
@@ -14,22 +14,22 @@
  * limitations under the License.
  */
 
-module "project-factory" {
-  source                  = "../../"
-  random_project_id       = true
-  name                    = "simple-sample-project"
-  org_id                  = var.organization_id
-  billing_account         = var.billing_account
-  default_service_account = "deprivilege"
+#module "project-factory" {
+#  source                  = "../../"
+#  random_project_id       = true
+#  name                    = "simple-sample-project"
+#  org_id                  = var.organization_id
+#  billing_account         = var.billing_account
+#  default_service_account = "deprivilege"
 
-  activate_api_identities = [{
-    api = "healthcare.googleapis.com"
-    roles = [
-      "roles/healthcare.serviceAgent",
-      "roles/bigquery.jobUser",
-    ]
-  }]
-}
+#  activate_api_identities = [{
+#    api = "healthcare.googleapis.com"
+#    roles = [
+#      "roles/healthcare.serviceAgent",
+#      "roles/bigquery.jobUser",
+#    ]
+#  }]
+#}
 module "test-vpc-module" {
   source       = "terraform-google-modules/network/google"
   version      = "~> 5.2"
