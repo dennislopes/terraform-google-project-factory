43d75f4d8a1f445514260be756756c3eef2b9868
dennislopes <dennislopes@gmail.com>
2023-02-16 00:17:58 -0300

    Update outputs.tf

--- examples/simple_project/outputs.tf
+++ examples/simple_project/outputs.tf
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
-output "project_info_example" {
-  value       = module.project-factory.project_id
-  description = "The ID of the created project"
-}
+#output "project_info_example" {
+#  value       = module.project-factory.project_id
+#  description = "The ID of the created project"
+#}
 
-output "domain_example" {
-  value       = module.project-factory.domain
-  description = "The organization's domain"
-}
+#output "domain_example" {
+#  value       = module.project-factory.domain
+#  description = "The organization's domain"
+#}
 
